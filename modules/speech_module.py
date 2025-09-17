7b247aaf1c663df05e93a4716d5291dbdf7677b4
Crichards27 <crichards27@cbastudents.org>
2025-09-17 09:02:29 -0400

    Update speech_module.py

--- modules/speech_module.py
+++ modules/speech_module.py
@@ -10,7 +10,7 @@ class SpeechModule:
         self._queue = queue.Queue()
         self._client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
         self._model = "tts-1"
-        self._voice = "fable"  # alloy, echo, fable, onyx, nova, shimmer
+        self._voice = "onyx"  # alloy, echo, fable, onyx, nova, shimmer
 
         bus.subscribe("audio.speak", self._on_speak)
         threading.Thread(target=self._loop, daemon=True).start()
